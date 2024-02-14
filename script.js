d8001616f5ca6bf9649d26cd9ec5c23e034727f0
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-02-14 13:59:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 
 function daysOfAYear(year) {
-	let year=prompt("Enter the year");
+	
 	if(year%4==0){
       return 366;
 	}else{
@@ -9,5 +9,5 @@ function daysOfAYear(year) {
 	 }
 	
 }
-
+let year=prompt("Enter the year");
 alert(daysOfAYear(year));
