9080f1882e3b04202d90f11fabf777eb6f38d875
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2025-10-24 14:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+// Prompt the user first
+let year = prompt('Please enter the year:');
+
+// Convert the input to a number
+year = Number(year);
+
+function leapyear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return 366; // leap year
+    } else {
+        return 365; // non-leap year
+    }
+}
+
+alert(leapyear(year));
